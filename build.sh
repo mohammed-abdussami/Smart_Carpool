3d804ca361c43e537158876b661f860beb738daf
Mohammed Abdus Sami <mohammedabdussami2003@gmail.com>
2026-05-23 20:28:36 +0530

    add auto superuser

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='160422733078@mjcollege.ac.in').exists() or User.objects.create_superuser('160422733078@mjcollege.ac.in', 'Admin@1234')" | python manage.py shell
